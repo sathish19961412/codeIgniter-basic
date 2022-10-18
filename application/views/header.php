f6ad071cec1cd29b2951db529b54fb3e6ae7dc69
sathish19961412 <sathishfreelanc5@gmail.com>
2022-10-18 09:56:53 +0530

    first view

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>page_title</title>
+</head>
+<body>
+
+<p>welcome to home</p>
+
+</body>
+</html>
---
+++ application/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>home</title>
+</head>
+<body>
+<form method="post" action="">
+	<input type="text" name="name">
+	<input type="submit" value="Save">
+</form>
+
+</body>
+</html>
